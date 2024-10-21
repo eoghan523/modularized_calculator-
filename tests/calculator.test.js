fa631210c005d75787d5cfdefa70df4c10487045
eoghan523 <eoghan523@gmail.com>
2024-10-21 17:06:52 +0100

    save changes

---
+++ tests/calculator.test.js
@@ -0,0 +1,20 @@
+
+import calculator from '../calculator.js'; 
+
+
+describe('calculator', () => {
+    test('adds 1 + 2 to equal 3', () => {
+        expect(calculator.add(1, 2)).toBe(3);
+
+    });
+
+
+
+
+
+
+
+
+
+
+});
