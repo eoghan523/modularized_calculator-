f2f9ddbae0ed83b3459b1c853f6a17d5e9cbcf60
eoghan523 <eoghan523@gmail.com>
2024-10-22 11:28:13 +0100

    Completed w5 challenge. The calculator was changed to ES6 using the arrow function which decreased the lines of code. Im calcculator.test.js I createed various test calculations ranging from adding and subtracting negative numbers to multiplication and division. I included a division by zero to test the error block.

--- calculator.js
+++ calculator.js
@@ -1,42 +1,20 @@
 
 import fs from 'fs';
 
-//Add function declared//
-function add(a, b) {
-    return a + b;
-    logCalculation(`${a} + ${b} = ${result}`);
-}
-//subtract function declared//
-function subtract(a, b) {
-    return a - b;
-    logCalculation(`${a} - ${b} = ${result}`);
-}
+//declare a calculator function//
+const calculator = {
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => { 
+        if (b === 0) {
+            throw new Error("Can't divide by 0...");
+        } else {
+            return a / b; // added return statement
 
-//Division function declared. If statement catches the error of dividing by 0. Else carrys out the division//
-function divide(a, b) {
-    if (b === 0) {
-     throw Error("You can divide by 0");
-
-    } else {  return a / b;
-        logCalculation(`${a} / ${b} = ${result}`);
+        }
+         
 
     }
-
 }
-//Multipy function declared//
-function multiply(a, b) {
-    return a * b;
-    logCalculation(`${a} * ${b} = ${result}`);
-}
-
-
-function logCalculation(calculation) {
-    fs.appendFile('calculations.txt', calculation + '\n', (err) => {
-        if (err) throw err("Could not be stored.");
-    });
-}
-
-//This allows for the functions defined above to be imported on other js scripts as a module. This is in line with "DRY" (Dont Repeat Yourself) principle.
-export { add, subtract, multiply, divide };
-
-
+module.exports = calculator;
--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,12 +1,40 @@
 
-import calculator from '../calculator.js'; 
-
+const calculator = require('../calculator');
 
 describe('calculator', () => {
     test('adds 1 + 2 to equal 3', () => {
         expect(calculator.add(1, 2)).toBe(3);
-
     });
+
+    test('subract 8 - 3 to equal 5', () => { 
+        expect(calculator.subtract(8, 3)).toBe(5);
+    });
+
+    test('multiply 5 * 6 to equal 30', () => {
+        expect(calculator.multiply(5, 6)).toBe(30);
+    });
+
+    test('divide 10 / 0 to throw  error', () => {
+        expect(() => calculator.divide(10, 0)).toThrow("Can't divide by 0...");
+    });
+
+    test('divide 12 / 3 equals 4', () => {
+        expect(calculator.divide(12, 3)).toBe(4);
+    });
+
+    test('add 100 + 450 to equal 550', () => {
+        expect(calculator.add(100, 450)).toBe(550);
+    });
+
+    test('subtracts -5 - -3 to equal -2', () => {
+        expect(calculator.subtract(-5, -3)).toBe(-2);
+    });
+
+    test('add -4 + -1 to equal -5', () => {
+        expect(calculator.add(-4, -1)).toBe(-5);
+    });
+
+    
 
 
 
